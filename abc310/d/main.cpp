3409f13fd5b83c7e8abde9d943ef416eba7e2f34
matcher9131 <matcher9131cd@gmail.com>
2023-07-17 11:11:17 +0900

    Update ABC310

--- abc310/d/main.cpp
+++ abc310/d/main.cpp
@@ -15,20 +15,51 @@
 using namespace std;
 using ll = long long;
 
+int n, t;
+// dis[i]のjビット目が1: iとjが相性が悪い
+vector<unsigned> dis;
+vector<unsigned> teams;
+unsigned search(unsigned currentPlayer) {
+    if (currentPlayer == n) {
+        return teams.size() == t ? 1U : 0;
+    }
+
+    unsigned answer = 0;
+
+    // 既にあるチームに追加
+    for (auto&& team : teams) {
+        if (!(team & dis[currentPlayer])) {
+            team ^= 1U << currentPlayer;
+            answer += search(currentPlayer + 1);
+            team ^= 1U << currentPlayer;
+        }
+    }
+
+    // 新しいチームに追加
+    if (teams.size() < t) {
+        teams.emplace_back(1U << currentPlayer);
+        answer += search(currentPlayer + 1);
+        teams.pop_back();
+    }
+
+    return answer;
+}
+
 int main() {
-    int n, t, m;
+    int m;
     cin >> n >> t >> m;
-    vector<vector<bool>> dis(n, vector<bool>(n));
+    dis = vector<unsigned>(n);
     for (int i = 0; i < m; ++i) {
         int a, b;
         cin >> a >> b;
         --a;
         --b;
-        dis[a][b] = true;
-        dis[b][a] = true;
+        dis[a] |= (1U << b);
+        dis[b] |= (1U << a);
     }
 
-    
+    teams.reserve(t);
+    cout << search(0) << endl;
 
     return 0;
 }
