c1b7cbaf0aed1e06d24e50138ccb281a8301f023
matcher9131 <matcher9131cd@gmail.com>
2023-09-17 09:51:08 +0900

    Fix ABC320C

--- abc320/c/main.cpp
+++ abc320/c/main.cpp
@@ -26,33 +26,26 @@ int main() {
     cin >> m;
     vector<string> s(3);
     cin >> s[0] >> s[1] >> s[2];
-
-    vector<vector<int>> pattern(10, vector<int>(3 * m));
     for (int i = 0; i < 3; ++i) {
-        for (int j = 0; j < m; ++j) {
-            pattern[(int)(s[i][j] - '0')][j] |= 1 << i;
-            pattern[(int)(s[i][j] - '0')][j + m] |= 1 << i;
-            pattern[(int)(s[i][j] - '0')][j + 2 * m] |= 1 << i;
-        }
+        s[i] += s[i] + s[i];
     }
-
-    vector<int> c(10);
-    vector<int> p(10);
-    auto getTime = [&](int d) {
-        for (int j = 0; j < 3 * m; ++j) {
-            if (pattern[d][j] > 0) {
-                ++c[d];
-                p[d] |= pattern[d][j];
-                if (c[d] >= 3 && p[d] == 7) return j;
-            }
-        }
-        return INF;
-    };
 
     int ans = INF;
-    for (int d = 0; d < 10; ++d) {
-        ans = min(ans, getTime(d));
-    }
+    vector<int> perm{ 0, 1, 2 };
+    do {
+        for (int d = 0; d < 10; ++d) {
+            int ansd = ([&](int d) {
+                int j = -1;
+                for (const int i : perm) {
+                    j = s[i].find('0' + (char)d, j + 1);
+                    if (j == -1) return INF;
+                }
+                return j;
+            })(d);
+            ans = min(ans, ansd);
+        }
+    } while (next_permutation(perm.begin(), perm.end()));
+
     cout << (ans == INF ? -1 : ans) << endl;
 
     return 0;
