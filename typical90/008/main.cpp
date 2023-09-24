668148be07396ee9c785b08023622bc7b09e2eb4
matcher9131 <matcher9131cd@gmail.com>
2023-09-24 11:32:02 +0900

    Add Typical90 008

---
+++ typical90/008/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+#include <cmath>
+#include <climits>
+#include <cfloat>
+#include <utility>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <cassert>
+#include <numeric>
+#include <functional>
+#include <atcoder/modint>
+using namespace std;
+using ll = long long;
+using mint = atcoder::modint1000000007;
+
+const string ATCODER = "atcoder";
+const int m = ATCODER.size();
+
+int main() {
+    int n;
+    string s;
+    cin >> n >> s;
+
+    vector<vector<mint>> dp(n + 1, vector<mint>(m + 1));
+    dp[0][0] = 1;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j <= m; ++j) {
+            dp[i + 1][j] += dp[i][j];
+            if (j < m && s[i] == ATCODER[j]) dp[i + 1][j + 1] += dp[i][j];
+        }
+    }
+    
+    cout << dp[n][m].val() << endl;
+
+    return 0;
+}
