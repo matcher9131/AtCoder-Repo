22b5c61ba3e568ee3b48c7c083a6e15a4eb010d2
matcher9131 <matcher9131cd@gmail.com>
2023-09-26 09:17:47 +0900

    Add Typical90 014

---
+++ typical90/014/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+#include <cmath>
+#include <climits>
+#include <cfloat>
+#include <utility>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <cassert>
+#include <numeric>
+#include <functional>
+using namespace std;
+using ll = long long;
+
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; ++i) {
+        cin >> b[i];
+    }
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+
+    ll ans = 0;
+    for (int i = 0; i < n; ++i) {
+        ans += abs(a[i] - b[i]);
+    }
+    cout << ans << endl;
+
+    return 0;
+}
