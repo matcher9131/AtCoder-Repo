2322da444d690d1f3601a6409bc1913026789115
matcher9131 <matcher9131cd@gmail.com>
2025-02-23 00:37:05 +0900

    Fix ABC394E and F

--- abc394/e/main.cpp
+++ abc394/e/main.cpp
@@ -19,6 +19,7 @@
 using namespace std;
 using ll = long long;
 
+constexpr int INF = 1e9;
 
 int main() {
     int n;
@@ -27,6 +28,40 @@ int main() {
     for (int i = 0; i < n; ++i) {
         cin >> s[i];
     }
+
+    vector<vector<int>> dist(n, vector<int>(n, INF));
+    queue<pair<int, int>> que;
+    for (int i = 0; i < n; ++i) {
+        que.push({ i, i });
+        dist[i][i] = 0;
+    }
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (i == j || s[i][j] == '-') continue;
+            que.push({ i, j });
+            dist[i][j] = 1;
+        }
+    }
+    while (!que.empty()) {
+        auto [x, y] = que.front();
+        que.pop();
+        for (int w = 0; w < n; ++w) {
+            for (int z = 0; z < n; ++z) {
+                if (dist[w][z] < INF) continue;
+                if (s[w][x] != '-' && s[y][z] != '-' && s[w][x] == s[y][z]) {
+                    dist[w][z] = dist[x][y] + 2;
+                    que.push({ w, z });
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cout << (dist[i][j] >= INF ? -1 : dist[i][j]) << " \n"[j == n - 1];
+        }
+    }
+    
 
     return 0;
 }
--- abc394/f/main.cpp
+++ abc394/f/main.cpp
@@ -33,48 +33,34 @@ int main() {
         g[b].push_back(a);
     }
 
-    vector<int> depth(n);
-    // depth, index
-    priority_queue<pair<int, int>> que;
-    function<void(int, int, int)> dfs = [&](int from, int parent, int d) -> void {
-        depth[from] = d;
-        que.emplace(d, from);
+    int ans = 0;
+    // dp[i]: 頂点iおよびその子孫のうちアルキル基として使える頂点数の最大値
+    vector<int> dp(n);
+    function<void(int,int)> dfs = [&](int from, int parent) -> void {
+        dp[from] = 1;
+        vector<int> dpc;
         for (const int to : g[from]) {
             if (to == parent) continue;
-            dfs(to, from, d + 1);
+            dfs(to, from);
+            dpc.push_back(dp[to]);
+        }
+        sort(dpc.begin(), dpc.end(), greater<int>());
+
+        // fromが水素原子のパターン
+        if (!dpc.empty()) {
+            ans = max(ans, dpc[0] + 1);
+        }
+        // fromが炭素原子のパターン
+        if (dpc.size() >= 4) {
+            ans = max(ans, dpc[0] + dpc[1] + dpc[2] + dpc[3] + 1);
+        }
+        // アルキル基の大きさを更新
+        if (dpc.size() >= 3) {
+            dp[from] = dpc[0] + dpc[1] + dpc[2] + 1;
         }
     };
-    dfs(0, -1, 0);
+    dfs(0, -1);
 
-    vector<int> dp(n);
-    while (!que.empty()) {
-        auto [_, v] = que.top();
-        que.pop();
-        if (g[v].size() < 4) {
-            dp[v] = 1;
-        } else {
-            vector<int> dpc;
-            for (const int child : g[v]) {
-                if (depth[v] < depth[child]) {
-                    dpc.push_back(dp[child]);
-                }
-            }
-            sort(dpc.begin(), dpc.end());
-            dp[v] = dpc[0] + dpc[1] + dpc[2] + 1;
-        }
-    }
-    if (g[0].size() < 4) {
-        dp[0] = dp[g[0][0]];
-    } else {
-        vector<int> dpc;
-        for (const int child : g[0]) {
-            dpc.push_back(dp[child]);
-        }
-        sort(dpc.begin(), dpc.end());
-        dp[0] = dpc[0] + dpc[1] + dpc[2] + dpc[3];
-    }
-
-    int ans = *max_element(dp.begin(), dp.end()) + 1;
     cout << (ans < 5 ? -1 : ans) << endl;
 
 
