aab98e1f1109ea25d0d4815fb807c5c730822682
matcher9131 <matcher9131cd@gmail.com>
2025-05-02 10:19:02 +0900

    Fix ABC350E

--- abc350/e/main.cpp
+++ abc350/e/main.cpp
@@ -21,7 +21,17 @@ using ll = long long;
 
 
 int main() {
-    
+    ll n, a;
+    double x, y;
+    cin >> n >> a >> x >> y;
 
+    unordered_map<ll, double> memo;
+    memo[0] = 0;
+    function<double(ll)> dfs = [&](ll i) {
+        if (memo.contains(i)) return memo[i];
+        return memo[i] = min(x + dfs(i / a), 1.2 * y + 0.2 * (dfs(i / 2) + dfs(i / 3) + dfs(i / 4) + dfs(i / 5) + dfs(i / 6)));
+    };
+
+    cout << setprecision(15) << dfs(n) << endl;
     return 0;
 }
