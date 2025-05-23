9b78417ba236282d39a8a59ed4b503655fad734f
matcher9131 <matcher9131cd@gmail.com>
2025-05-23 11:35:40 +0900

    Add ABC345F

--- abc345/f/main.cpp
+++ abc345/f/main.cpp
@@ -21,7 +21,55 @@ using ll = long long;
 
 
 int main() {
-    
+    int n, m, k;
+    cin >> n >> m >> k;
+    if (k % 2) {
+        cout << "No" << endl;
+        return 0;
+    }
+
+    vector<vector<pair<int, int>>> g(n);
+    for (int i = 0; i < m; ++i) {
+        int u, v;
+        cin >> u >> v;
+        --u;
+        --v;
+        g[u].emplace_back(v, i);
+        g[v].emplace_back(u, i);
+    }
+
+    vector<bool> visited(n);
+    vector<bool> on(n);
+    int numOn = 0;
+    vector<int> ans;
+    function<void(int)> dfs = [&](int from) -> void {
+        visited[from] = true;
+        for (const auto& [to, ei] : g[from]) {
+            if (visited[to]) continue;
+            dfs(to);
+            if (!on[to] && numOn < k) {
+                numOn -= (on[from] ? 1 : 0) + (on[to] ? 1 : 0);
+                on[from] = !on[from];
+                on[to] = !on[to];
+                numOn += (on[from] ? 1 : 0) + (on[to] ? 1 : 0);
+                ans.push_back(ei);
+            }
+        }
+    };
+    for (int i = 0; i < n; ++i) {
+        if (visited[i]) continue;
+        dfs(i);
+    }
+
+    if (numOn == k) {
+        cout << "Yes" << endl;
+        cout << ans.size() << endl;
+        for (int i = 0; i < (int)ans.size(); ++i) {
+            cout << ans[i] + 1 << " \n"[i == (int)ans.size() - 1];
+        }
+    } else {
+        cout << "No" << endl;
+    }
 
     return 0;
 }
