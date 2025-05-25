1271c01b8dc1e0abc3d95de2f691b9f02c7f41d7
matcher9131 <matcher9131cd@gmail.com>
2025-05-25 09:23:47 +0900

    Add ABC407F

--- abc407/f/main.cpp
+++ abc407/f/main.cpp
@@ -21,9 +21,47 @@ using namespace std;
 using ll = long long;
 
 
-
 int main() {
-    
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+
+    vector<pair<ll, int>> aDesc(n);
+    for (int i = 0; i < n; ++i) {
+        aDesc[i] = {a[i], i};
+    }
+    sort(aDesc.begin(), aDesc.end(), greater<pair<ll, int>>());
+
+    vector<int> l(n), r(n);
+    set<int> used{-1, n};
+    for (const auto &[_, i] : aDesc) {
+        auto itr = used.lower_bound(i);
+        auto itl = itr;
+        --itl;
+        l[i] = i - *itl - 1;
+        r[i] = *itr - i - 1;
+        used.insert(i);
+    }
+
+    vector<ll> b(n+2);
+    for (int i = 0; i < n; ++i) {
+        b[0] += a[i];
+        b[l[i] + 1] -= a[i];
+        b[r[i] + 1] -= a[i];
+        b[l[i] + r[i] + 2] += a[i];
+    }
+    for (int c = 0; c < 2; ++c) {
+        for (int j = 1; j < n+2; ++j) {
+            b[j] += b[j-1];
+        }
+    }
+
+    for (int k = 0; k < n; ++k) {
+        cout << b[k] << endl;
+    }
     
     return 0;
 }
