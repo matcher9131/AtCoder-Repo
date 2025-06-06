6825e46c9a4b1eb3ffac70426173d9c62c67e1d2
matcher9131 <matcher9131cd@gmail.com>
2025-06-06 22:31:01 +0900

    Fix ABC340F

--- abc340/f/main.cpp
+++ abc340/f/main.cpp
@@ -19,47 +19,43 @@
 using namespace std;
 using ll = long long;
 
-ll extGcd(ll a, ll b, ll &x, ll &y) {
-    if (b == 0) {
-        x = 1;
-        y = 0;
-        return a;
-    }
-    ll d = extGcd(b, a%b, y, x);
-    y -= a/b * x;
-    return d;
+pair<ll, ll> ext_gcd(ll a, ll b) {
+    if (b == 0) return { 1LL, 0 };
+    auto [y, x] = ext_gcd(b, a % b);
+    y -= a / b * x;
+    return { x, y };
 }
 
 int main() {
     ll a, b;
     cin >> a >> b;
+    
     if (a == 0) {
-        if (b == 2 || b == -2) {
-            cout << 1 << " " << 0 << endl;
-            return 0;
+        if (abs(b) <= 2) {
+            cout << 2 / abs(b) << " " << 0 << endl;
         } else {
             cout << -1 << endl;
-            return 0;
         }
+        return 0;
     } else if (b == 0) {
-        if (a == 2 || a == -2) {
-            cout << 0 << " " << 1 << endl;
-            return 0;
+        if (abs(a) <= 2) {
+            cout << 0 << " " << 2 / abs(a) << endl;
         } else {
             cout << -1 << endl;
-            return 0;
         }
+        return 0;
     }
 
     ll g = gcd(a, b);
-    if (g != 1 && g != 2) {
+    if (g > 2) {
         cout << -1 << endl;
         return 0;
     }
 
-    ll x, y;
-    extGcd(b, -a, x, y);
-    cout << 2 * x << " " << 2 * y << endl;
+    auto [x, y] = ext_gcd(b, -a);
+    x = x * 2 / g;
+    y = y * 2 / g;
+    cout << x << " " << y << endl;
 
     return 0;
 }
