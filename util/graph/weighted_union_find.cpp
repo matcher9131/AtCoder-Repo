a449c6cb3f3bf7354f8b417f438aa1317ad9c3e7
matcher9131 <matcher9131cd@gmail.com>
2025-06-23 13:16:57 +0900

    Add Weighted Union Find to util

---
+++ util/graph/weighted_union_find.cpp
@@ -0,0 +1,56 @@
+#include <vector>
+#include <stack>
+#include <utility>
+#include <functional>
+using namespace std;
+
+template<
+    class S = int,
+    auto op = [](S a, S b) { return a + b; },
+    auto inv_op = [](S a, S b) { return a - b; },
+    auto e = []() { return 0; }
+> class UnionFind {
+    static_assert(is_convertible_v<decltype(op), function<S(S, S)>>);
+    static_assert(is_convertible_v<decltype(inv_op), function<S(S, S)>>);
+    static_assert(is_convertible_v<decltype(e), function<S()>>);
+
+    vector<int> _parent;
+    vector<S> _value;
+
+public:
+    UnionFind(int n) : _parent(n, -1), _value(n, e()) {}
+
+    int get_root(int i) {
+        stack<int> s;
+        while (_parent[i] >= 0) {
+            s.push(i);
+            i = _parent[i];
+        }
+        S val = e();
+        while (!s.empty()) {
+            int j = s.top();
+            s.pop();
+            val = op(val, _value[j]);
+            _value[j] = val;
+            _parent[j] = i;
+        }
+        return i;
+    }
+
+    int get_size(int i) {
+        return -_parent[get_root(i)];
+    }
+
+    int connect(int i, int j, S d) {
+        int ri = get_root(i);
+        int rj = get_root(j);
+        if (ri == rj) {
+            if (inv_op(_value[j], _value[i]) != d) return -1;
+            return 0;
+        }
+        _parent[ri] += _parent[rj];
+        _parent[rj] = ri;
+        _value[rj] = inv_op(op(_value[i], d), _value[j]);
+        return 1;
+    }
+};
