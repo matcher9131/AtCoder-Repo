9e79cd574b79c4a12045621014cfdfe40fda4c39
matcher9131 <matcher9131cd@gmail.com>
2025-06-29 10:31:08 +0900

    Add ABC412E

--- abc412/e/main.cpp
+++ abc412/e/main.cpp
@@ -21,7 +21,40 @@ using ll = long long;
 
 
 int main() {
-    
+    ll l, r;
+    cin >> l >> r;
 
+    ll m = 1e7;
+    // √R以下が素数かどうか
+    vector<bool> isPrime(m+1, true);
+    isPrime[0] = isPrime[1] = false;
+    // √R以下の素数を素因数に持つかどうか
+    vector<bool> visited(r - l);
+
+    ll ans = 1; // Lは必ず対象
+    for (ll p = 2; p * p <= r; ++p) {
+        // エラトステネスの篩（通常版）
+        if (!isPrime[p]) continue;
+        for (ll x = 2*p; x <= m; x += p) {
+            isPrime[x] = false;
+        }
+
+        // エラトステネスの区間篩
+        // start: Aより大きく最小のpの倍数
+        ll start = (l / p + 1) * p;
+        for (ll x = start; x <= r; x += p) {
+            if (visited[x - (l+1)]) continue;
+            visited[x - (l+1)] = true;
+            ll y = x;
+            while (y % p == 0) y /= p;
+            if (y == 1) ++ans;
+        }
+    }
+
+    for (const bool b : visited) {
+        if (!b) ++ans;
+    }
+
+    cout << ans << endl;
     return 0;
 }
