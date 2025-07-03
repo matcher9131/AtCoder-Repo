00c74015021658513b7d6f0f85362bb19d6a7263
matcher9131 <matcher9131cd@gmail.com>
2025-07-03 11:14:17 +0900

    Add ABC335F

--- abc335/f/main.cpp
+++ abc335/f/main.cpp
@@ -16,12 +16,44 @@
 #include <cassert>
 #include <numeric>
 #include <functional>
+#include <atcoder/modint>
 using namespace std;
 using ll = long long;
+using mint = atcoder::modint998244353;
 
+constexpr int BORDER = 450;
 
 int main() {
-    
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+
+    vector<mint> dp(n);
+    dp[0] = 1;
+    vector<vector<mint>> dp2(BORDER+1, vector<mint>(BORDER));
+
+    for (int i = 0; i < n; ++i) {
+        for (int d = 1; d <= BORDER; ++d) {
+            dp[i] += dp2[d][i%d];
+        }
+
+        if (a[i] > BORDER) {
+            for (int j = i + a[i]; j < n; j += a[i]) {
+                dp[j] += dp[i];
+            }
+        } else {
+            dp2[a[i]][i % a[i]] += dp[i];
+        }
+    }
+
+    mint ans = 0;
+    for (int i = 0; i < n; ++i) {
+        ans += dp[i];
+    }
+    cout << ans.val() << endl;
 
     return 0;
 }
