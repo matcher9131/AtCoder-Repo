c4cf871be3a699132ea383aecb9298a36861a8ab
matcher9131 <matcher9131cd@gmail.com>
2025-07-11 13:25:18 +0900

    Fix ABC332E

--- abc332/e/main.cpp
+++ abc332/e/main.cpp
@@ -30,35 +30,32 @@ int main() {
     sort(w.begin(), w.end(), greater<double>());
     double avg = accumulate(w.begin(), w.end(), 0.0) / d;
 
-    priority_queue<pair<double, double>> bags;
-    for (int i = 0; i < d; ++i) {
-        bags.emplace(avg * avg, 0);
+    // dp[S][j]: グッズの集合がSでそれらをj袋に分けたときにおける分散の2乗の最小値
+    // （ただしここで言う分散は平均値として全D袋の平均値を用いる）
+    vector<vector<double>> dp(1 << n, vector<double>(d+1, 1e30));
+
+    // dp[S][1]
+    for (int bit = 0; bit < 1 << n; ++bit) {
+        double ws = 0;
+        for (int i = 0; i < n; ++i) {
+            if (bit & (1 << i)) ws += w[i];
+        }
+        dp[bit][1] = (ws - avg) * (ws - avg);
     }
-    vector<double> bagsFinished;
-    
-    for (int i = 0; i < n; ++i) {
-        auto [pd, sum] = bags.top();
-        bags.pop();
-        if (sum + w[i] > avg) {
-            bagsFinished.push_back(sum + w[i]);
-        } else {
-            double ns = sum + w[i];
-            bags.emplace((ns - avg) * (ns - avg), ns);
+
+    for (int j = 2; j <= d; ++j) {
+        for (int bit = 0; bit < 1 << n; ++bit) {
+            int subBit = bit;
+            while (true) {
+                dp[bit][j] = min(dp[bit][j], dp[bit ^ subBit][j-1] + dp[subBit][1]);
+                if (subBit == 0) break;
+                subBit = (subBit - 1) & bit;
+            }
         }
     }
-    while (!bags.empty()) {
-        auto [_, sum] = bags.top();
-        bags.pop();
-        bagsFinished.push_back(sum);
-    }
 
-    double ans = 0;
-    for (int i = 0; i < d; ++i) {
-        printf("bag[%d] = %lf\n", i, bagsFinished[i]);
-        ans += (bagsFinished[i] - avg) * (bagsFinished[i] - avg);
-    }
-    ans /= d;
-
+    double ans = dp[(1 << n) - 1][d] / d;
     cout << setprecision(15) << ans << endl;
+
     return 0;
 }
