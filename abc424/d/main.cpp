0dfcf298226c89576c50b1b6e862d3cfebf47caf
matcher9131 <matcher9131cd@gmail.com>
2025-09-21 10:52:58 +0900

    Fix ABC424D

--- abc424/d/main.cpp
+++ abc424/d/main.cpp
@@ -6,6 +6,10 @@ using ull = unsigned long long;
 using mint = atcoder::modint998244353;
 constexpr ll INF = 1e18;
 
+inline bool isBlack(ll board, ll i) {
+    return board & (1ULL << i);
+}
+
 void solve() {
     ll h, w;
     cin >> h >> w;
@@ -19,9 +23,22 @@ void solve() {
     }
     ull mask = (1ULL << (w+1)) - 1ULL;
 
-    vector<vector<ll>> dp(h*w, vector<ll>(1<<(w+1), INF));
+    vector<vector<ll>> dp(h*w+1, vector<ll>(1<<(w+1), INF));
+    dp[w+1][master & mask] = 0;
+    for (ll i = w+1; i < h*w; ++i) {
+        ull masked = (master >> (i-w-1)) & mask;
+        for (ull bit = 0; bit < 1ULL << (w+1); ++bit) {
+            if ((masked & bit) != bit) continue;
+            // 白
+            dp[i+1][bit>>1] = min(dp[i+1][bit>>1], dp[i][bit] + (isBlack(master, i) ? 1 : 0));
+            // 黒
+            if (isBlack(master, i) && (i%w == 0 || !isBlack(bit, 0) || !isBlack(bit, 1) || !isBlack(bit, w))) {
+                dp[i+1][(bit>>1) | (1ULL<<w)] = min(dp[i+1][(bit>>1) | (1ULL<<w)], dp[i][bit]);
+            }
+        }
+    }
 
-    // 
+    cout << *min_element(dp[h*w].begin(), dp[h*w].end()) << endl;
 }
 
 int main() {
