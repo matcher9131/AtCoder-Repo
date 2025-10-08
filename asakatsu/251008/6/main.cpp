f5fe3a959b05122f18485de1c987584d6b7408ac
matcher9131 <matcher9131cd@gmail.com>
2025-10-08 22:23:40 +0900

    Add あさかつ251008-6

--- asakatsu/251008/6/main.cpp
+++ asakatsu/251008/6/main.cpp
@@ -13,17 +13,48 @@ int main() {
     cin >> n;
     string s, t;
     cin >> s >> t;
+    ll sLen = s.size();
 
     vector<vector<ll>> m(26);
     for (ll i = 0; i < (ll)s.size(); ++i) {
         m[s[i] - 'a'].push_back(i);
     }
+    for (ll i = 0; i < (ll)s.size(); ++i) {
+        m[s[i] - 'a'].push_back(i+sLen);
+    }
 
-    auto isOK = [&](ll k) -> bool {
-        for (ll ti = 0; ti < (ll)t.size(); ++ti) {
-            if ()
+    auto f = [&](ll c, ll i, ll j) -> ll {
+        if (m[c].empty()) return INF;
+        ll iq = i / sLen;
+        ll ir = i % sLen;
+        ll mLen = (ll)m[c].size() / 2;
+        ll jq = j / mLen;
+        ll jr = j % mLen;
+        if (jr == 0) {
+            --jq;
+            jr = mLen;
         }
+        return *(lower_bound(m[c].begin(), m[c].end(), ir) + (jr - 1)) + (iq + jq) * sLen;
     };
+
+    auto isOK = [&](ll x) -> bool {
+        ll cur = -1;
+        for (const char ch : t) {
+            cur = f(ch - 'a', cur+1, x);
+            if (cur >= n*sLen) return false;
+        }
+        return true;
+    };
+
+    ll ok = 0;
+    ll ng = INF;
+    while (ng - ok > 1) {
+        ll mid = (ok + ng) / 2;
+        if (isOK(mid)) ok = mid;
+        else ng = mid;
+    }
+    cout << ok << endl;
+
 
     return 0;
 }
