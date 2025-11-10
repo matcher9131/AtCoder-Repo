4c02aab177bf7627e7c65abf3db8a94e709e4dbc
matcher9131 <matcher9131cd@gmail.com>
2025-11-10 21:44:03 +0900

    Add あさかつ251110-2,3

--- asakatsu/251110/2/main.cpp
+++ asakatsu/251110/2/main.cpp
@@ -9,7 +9,31 @@ template<typename T> inline void chmin(T &x, T y) { if (y < x) x = y; }
 template<typename T> inline void chmax(T &x, T y) { if (x < y) x = y; }
 
 int main() {
-    
+    ll n;
+    cin >> n;
+    vector<vector<ll>> a(2, vector<ll>(n));
+    for (ll i = 0; i < 2; ++i) {
+        for (ll j = 0; j < n; ++j) {
+            cin >> a[i][j];
+        }
+    }
+
+    vector<ll> s1(n);
+    s1[0] = a[0][0];
+    for (ll i = 1; i < n; ++i) {
+        s1[i] = s1[i-1] + a[0][i];
+    }
+    vector<ll> s2(n);
+    s2[n-1] = a[1][n-1];
+    for (ll i = n-2; i >= 0; --i) {
+        s2[i] = s2[i+1] + a[1][i];
+    }
+
+    ll ans = 0;
+    for (ll i = 0; i < n; ++i) {
+        chmax(ans, s1[i] + s2[i]);
+    }
+    cout << ans << endl;
 
     return 0;
 }
--- asakatsu/251110/3/main.cpp
+++ asakatsu/251110/3/main.cpp
@@ -9,7 +9,35 @@ template<typename T> inline void chmin(T &x, T y) { if (y < x) x = y; }
 template<typename T> inline void chmax(T &x, T y) { if (x < y) x = y; }
 
 int main() {
-    
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+
+    ll ans = 0;
+    stack<pair<ll, ll>> st;
+    for (const ll x : a) {
+        ++ans;
+        if (st.empty()) {
+            st.emplace(x, 1);
+        } else {
+            auto [v, c] = st.top();
+            if (x == v) {
+                st.pop();
+                ++c;
+                if (c < x) {
+                    st.emplace(v, c);
+                } else {
+                    ans -= v;
+                }
+            } else {
+                st.emplace(x, 1);
+            }
+        }
+        cout << ans << "\n";
+    }
 
     return 0;
 }
